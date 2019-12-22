088c176fb0ecbc013ea84daea264bb8006ea6e76
Numinous <xnuminousx@gmail.com>
2019-12-22 17:19:08 -0600

    update version & added support for new command

--- src/main/java/me/numin/elementalcompanions/commands/HelpCommand.java
+++ src/main/java/me/numin/elementalcompanions/commands/HelpCommand.java
@@ -6,7 +6,8 @@ class HelpCommand {
 
     HelpCommand(CommandSender sender) {
         sender.sendMessage("/ec spawn [element] - Spawns a new companion for the desired element!");
-        sender.sendMessage("/ec reactive [true/false] - Set your companion to be reactive or not!");
         sender.sendMessage("/ec remove - Remove your companion :(");
+        sender.sendMessage("/ec reactive [true/false] - Set your companion to be reactive or not!");
+        sender.sendMessage("/ec silence [true/false] - Silence your companion or hear them roar!");
     }
 }
